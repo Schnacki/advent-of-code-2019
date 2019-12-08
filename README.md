2b9a30d9d74529b8f4251bbc7c872162b291d1ac
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2019-12-08 12:51:54 +0100

    Partly solving Part 2 of Day 6 (wip)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# advent-of-code-2019
+# Advent of Code 2019
+
+Trying to solve the **Advent of Code 2019** [Excercises](https://adventofcode.com/2019) in a fully functional way in haskell...
--- app/Day6.hs
+++ app/Day6.hs
@@ -1,6 +1,8 @@
 module Day6 (part1) where
 
-data Object = Orbit String [Object] deriving Show
+import Data.List(find)
+
+data Object = Orbit String [Object] deriving (Show, Eq)
 
 findAll :: (Eq a) => a -> [(a,b)] -> [b]
 findAll search = foldr (\(a,b) -> (\list -> if a == search then b:list else list)) []
@@ -14,9 +16,20 @@ buildOrbitTree list val = Orbit val $ map (\p -> buildOrbitTree list p) (findAll
 countOrbits :: Object -> Int -> Int
 countOrbits (Orbit _ objects) level = level + (sum $ map (\object -> countOrbits object (level+1)) objects)
 
+generateObjectTreeFromFile :: String -> FilePath -> IO Object
+generateObjectTreeFromFile root file = do
+    lines <- lines <$> readFile file
+    return $ buildOrbitTree (parseString <$> lines) root
+
 part1 :: FilePath -> IO Int
 part1 file = do
-    lines <- lines <$> readFile file
-    let orbits = parseString <$> lines
-    let tree = buildOrbitTree orbits "COM"
+    tree <- generateObjectTreeFromFile "COM" file
     return $ countOrbits tree 0
+
+    
+findPathToObject :: String -> Object -> [String]
+findPathToObject name object@(Orbit n objects)
+    | name == n = [n]
+    | otherwise = case (find ((/=) []) $ map (findPathToObject name) objects) of
+        Nothing -> []
+        Just l -> n:l
